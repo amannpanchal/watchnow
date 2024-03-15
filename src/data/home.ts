5eae38648c3dbed107e02504dcde47e0a46c73d9
Aman <amanpanchal8397@gmail.com>
2024-03-15 13:37:43 +0530

    done
    done

---
+++ src/data/home.ts
@@ -0,0 +1,180 @@
+export const categories = [
+  "All",
+  "JavaScript",
+  "TypeScript",
+  "Programming",
+  "Weight Lifting",
+  "Bowling",
+  "Hiking",
+  "React",
+  "Next.js",
+  "Functional Programming",
+  "Object Oriented Programming",
+  "Frontend Web Development",
+  "Backend Web Development",
+  "Web Development",
+  "Coding",
+]
+
+export const videos = [
+  {
+    id: "1",
+    title: "How to Build a Weather App with React",
+    channel: {
+      name: "Traversy Media",
+      id: "TraversyMedia",
+      profileUrl:
+        "https://yt3.ggpht.com/ytc/APfAmoGQjlZMnG_h4n6JtHw3FhDmPSJ2Ltkf3dIkiB3x=s48-c-k-c0x00ffffff-no-rj",
+    },
+    views: 125000,
+    postedAt: new Date("2023-05-15"),
+    duration: 600,
+    thumbnailUrl: "https://i.ytimg.com/vi/qWPtKtYEsN4/maxresdefault.jpg",
+    videoUrl:
+      "https://www.youtube.com/watch?v=qWPtKtYEsN4.mp4",
+  },
+  {
+    id: "2",
+    title: "Responsive Web Design Tutorial - Full Course",
+    channel: {
+      name: "freeCodeCamp.org",
+      id: "freeCodeCamp",
+      profileUrl:
+        "https://yt3.ggpht.com/ytc/APfAmoGY07F5ikRn6OM4pD6o5IaDQYnDTKYX-9ocmJwF=s48-c-k-c0x00ffffff-no-rj",
+    },
+    views: 750000,
+    postedAt: new Date("2023-01-20"),
+    duration: 1500,
+    thumbnailUrl: "https://i.ytimg.com/vi/gQojMIhELvM/maxresdefault.jpg",
+    videoUrl:
+      "https://www.youtube.com/watch?v=gQojMIhELvM.mp4",
+  },
+  {
+    id: "3",
+    title: "Learn JavaScript in One Hour",
+    channel: {
+      name: "Programming with Mosh",
+      id: "ProgrammingwithMosh",
+      profileUrl:
+        "https://yt3.ggpht.com/ytc/APfAmoFk2hWfiC28mXeYD68yRzj1Bn6c4B2eKNHZBqJH=s48-c-k-c0x00ffffff-no-rj",
+    },
+    views: 380000,
+    postedAt: new Date("2023-02-10"),
+    duration: 3600,
+    thumbnailUrl: "https://i.ytimg.com/vi/W6NZfCO5SIk/maxresdefault.jpg",
+    videoUrl:
+      "https://www.youtube.com/watch?v=W6NZfCO5SIk.mp4",
+  },
+  {
+    id: "4",
+    title: "Python Crash Course for Beginners",
+    channel: {
+      name: "Corey Schafer",
+      id: "CoreySchafer",
+      profileUrl:
+        "https://yt3.ggpht.com/ytc/APfAmoH_t91AxVW2ek3jTQZ9IgBsGPjCtLClEK0Sy6EE7w=s48-c-k-c0x00ffffff-no-rj",
+    },
+    views: 200000,
+    postedAt: new Date("2023-06-30"),
+    duration: 2400,
+    thumbnailUrl: "https://i.ytimg.com/vi/rfscVS0vtbw/maxresdefault.jpg",
+    videoUrl:
+      "https://www.youtube.com/watch?v=rfscVS0vtbw.mp4",
+  },
+  {
+    id: "5",
+    title: "Introduction to Machine Learning",
+    channel: {
+      name: "Sentdex",
+      id: "sentdex",
+      profileUrl:
+        "https://yt3.ggpht.com/ytc/AAUvwnh2MggccmtYjBm8Aa1zmF7TDJiVl2aT7-3IBIHF=s48-c-k-c0x00ffffff-no-rj",
+    },
+    views: 300000,
+    postedAt: new Date("2023-09-01"),
+    duration: 1800,
+    thumbnailUrl: "https://i.ytimg.com/vi/gZmobeGL0Yg/maxresdefault.jpg",
+    videoUrl:
+      "https://www.youtube.com/watch?v=gZmobeGL0Yg.mp4",
+  },
+  {
+    id: "6",
+    title: "Advanced CSS Layout Techniques",
+    channel: {
+      name: "Layout Land",
+      id: "LayoutLand",
+      profileUrl:
+        "https://yt3.ggpht.com/ytc/AAUvwngKPWQ7DxKZ4q3yiq2pzgMfwb2-B35hBYow-Y7x=s48-c-k-c0x00ffffff-no-rj",
+    },
+    views: 150000,
+    postedAt: new Date("2023-07-10"),
+    duration: 2400,
+    thumbnailUrl: "https://i.ytimg.com/vi/7kVeCqQCxlk/maxresdefault.jpg",
+    videoUrl:
+      "https://www.youtube.com/watch?v=7kVeCqQCxlk.mp4",
+  },
+  {
+    id: "7",
+    title: "Node.js Crash Course",
+    channel: {
+      name: "Academind",
+      id: "Academind",
+      profileUrl:
+        "https://yt3.ggpht.com/ytc/AAUvwnjeiAeQt8VRo5V_z_EzqCShTKTVCFzMQYmyzBJMeA=s48-c-k-c0x00ffffff-no-rj",
+    },
+    views: 500000,
+    postedAt: new Date("2023-04-05"),
+    duration: 1800,
+    thumbnailUrl: "https://i.ytimg.com/vi/fBNz5xF-Kx4/maxresdefault.jpg",
+    videoUrl:
+      "https://www.youtube.com/watch?v=fBNz5xF-Kx4.mp4",
+  },
+  {
+    id: "8",
+    title: "Understanding Redux - Tutorial for Beginners",
+    channel: {
+      name: "The Net Ninja",
+      id: "TheNetNinja",
+      profileUrl:
+        "https://yt3.ggpht.com/ytc/AAUvwng4jJw1e4mlIwziYsgpKDQy5_a8vXAlpqOH9x2EqQ=s48-c-k-c0x00ffffff-no-rj",
+    },
+    views: 280000,
+    postedAt: new Date("2023-08-20"),
+    duration: 1200,
+    thumbnailUrl: "https://i.ytimg.com/vi/poQXNp9ItL4/maxresdefault.jpg",
+    videoUrl:
+      "https://www.youtube.com/watch?v=poQXNp9ItL4.mp4",
+  },
+  {
+    id: "9",
+    title: "Python Django Tutorial for Beginners",
+    channel: {
+      name: "CodeWithHarry",
+      id: "CodeWithHarry",
+      profileUrl:
+        "https://yt3.ggpht.com/ytc/AAUvwni4C5hBDU2_NpQQGx-nkytYlAxYNzCyJi5rgw=s48-c-k-c0x00ffffff-no-rj",
+    },
+    views: 400000,
+    postedAt: new Date("2023-03-10"),
+    duration: 2700,
+    thumbnailUrl: "https://i.ytimg.com/vi/_uQrJ0TkZlc/maxresdefault.jpg",
+    videoUrl:
+      "https://www.youtube.com/watch?v=_uQrJ0TkZlc.mp4",
+  },
+  {
+    id: "10",
+    title: "React Native Tutorial for Beginners",
+    channel: {
+      name: "CodeWithChris",
+      id: "CodeWithChris",
+      profileUrl:
+        "https://yt3.ggpht.com/ytc/AAUvwnjGnvjzg-m1mkZMNeQNLGWS0KWquEWhvTF9KzN3Dw=s48-c-k-c0x00ffffff-no-rj",
+    },
+    views: 220000,
+    postedAt: new Date("2023-11-05"),
+    duration: 2100,
+    thumbnailUrl: "https://i.ytimg.com/vi/QzVnWu_zbBc/maxresdefault.jpg",
+    videoUrl:
+      "https://www.youtube.com/watch?v=QzVnWu_zbBc.mp4",
+  },
+];
---
+++ src/data/sidebar.ts
@@ -0,0 +1,34 @@
+export const subscriptions = [
+  {
+    channelName: "Random",
+    id: "Fireship",
+    imgUrl:
+      "https://api.slingacademy.com/public/sample-photos/20.jpeg",
+  },
+  {
+    channelName: "Abc",
+    id: "CalebCurry",
+    imgUrl:
+      "https://api.slingacademy.com/public/sample-photos/6.jpeg",
+  },
+  {
+    channelName: "Hello guys",
+    id: "KevinPowell",
+    imgUrl:
+      "https://api.slingacademy.com/public/sample-photos/1.jpeg",
+  },
+  {
+    channelName: "Well to know",
+    id: "TraversyMedia",
+    imgUrl:
+      "https://api.slingacademy.com/public/sample-photos/5.jpeg",
+  },
+]
+
+export const playlists = [
+  { id: "1", name: "Frontend & Backend" },
+  { id: "2", name: "Favorites" },
+  { id: "3", name: "React" },
+  { id: "4", name: "Non-Dev" },
+  { id: "5", name: "TypeScript" },
+]
